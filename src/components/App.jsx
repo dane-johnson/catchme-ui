79f5b6fcf391a7c7e7e74e56c8b787a6a6d008c8
dane <dane_johnson@ymail.com>
2017-02-07 23:27:05 -0600

    Added bootstrap and beefed up the App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { PageHeader, Button } from 'react-bootstrap'
 
-const App = () => (
-  <h1>Hello World!</h1>
+const App = (props) => (
+  <div>
+    <style>{`
+      .centered{
+        text-align: center
+      }
+      `}</style>
+    <div className="centered">
+      <PageHeader>Catch@Me</PageHeader>
+      <p><em>{`
+        Catch@Me is the next ARG trend. Also I made it to fuck around with my
+        friends.
+        `}</em></p>
+      <Button onClick={props.onPress}>Start!</Button>
+    </div>
+  </div>
 )
 
 export default App;
