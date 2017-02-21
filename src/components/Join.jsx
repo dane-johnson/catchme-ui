a22216dc8c39a2c2646ca37276410ba0b93242a6
Dane <dane_johnson@ymail.com>
2017-02-20 20:06:34 -0600

    Added cs and got the fuck off that VM

--- src/components/Join.jsx
+++ src/components/Join.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
+import '../css/centered.css'
+
 const Join = () => (
-  <h1>Join Game!</h1>
+  <div className="centered">
+    <h1>Join Game!</h1>
+  </div>
 );
 
 export default Join
