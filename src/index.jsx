8e4b031d276942d36432f960c180bd18b34aa44b
Dane Johnson <dane_johnson@ymail.com>
2017-02-21 21:51:16 -0600

    Reverted index

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App.jsx'
 
-//TODO Point entry back at APP before merge to master
-import Lobby from './components/Lobby.jsx'
-
-const players = ["Dane", "Cole"]
-
 ReactDOM.render(
-  <Lobby roomCode={"HOLA"} leader={false} players={players}/>,
+  <App />,
   document.getElementById('root')
 );
