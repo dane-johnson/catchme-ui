18bb0381c9c7ab3756307d76a2980367ba6017c7
Dane Johnson <dane_johnson@ymail.com>
2017-02-26 14:17:28 -0600

    Removed redux debugger in PROD

--- src/index.js
+++ src/index.js
@@ -6,10 +6,16 @@ import reducer from './reducers';
 import App from './App';
 import './index.css';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+let store;
+if (process.env.NODE_ENV === "DEV") {
+  store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
+}
+else {
+  store = createStore(reducer)
+}
 
 ReactDOM.render(
   <Provider store={store}>
