4b6aea01faac76938b9da8cdfae7abb6e8fca677
Dane Johnson <dane_johnson@ymail.com>
2017-03-05 15:53:02 -0600

    Made center a state variable

--- src/components/NavScreen.js
+++ src/components/NavScreen.js
@@ -3,17 +3,25 @@ import { Map, Marker, TileLayer } from 'react-leaflet'
 import '../css/map.css'
 
 class NavScreen extends React.Component{
+  constructor(props) {
+    super(props);
+    const { position } = props;
+    this.state = {
+      center: [position.coords.latitude, position.coords.longitude]
+    }
+  }
   render() {
     const { position } = this.props;
+    const { center } = this.state;
     if (!position) return null;
-    const center = [position.coords.latitude, position.coords.longitude]
+    const me = [position.coords.latitude, position.coords.longitude]
     return (
       <Map center={center} zoom={20}>
         <TileLayer
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         />
-        <Marker position={center}/>
+        <Marker position={me}/>
       </Map>
     )
   }
