b7db286df29fc4dc0051c3fbce63f68fa9bfb28c
Dane Johnson <dane_johnson@ymail.com>
2017-03-05 15:41:54 -0600

    refactored navscreen to component

--- src/components/NavScreen.js
+++ src/components/NavScreen.js
@@ -2,18 +2,21 @@ import React from 'react'
 import { Map, Marker, TileLayer } from 'react-leaflet'
 import '../css/map.css'
 
-const NavScreen = ({position}) => {
-  if (!position) return null;
-  const center = [position.coords.latitude, position.coords.longitude]
-  return (
-    <Map center={center} zoom={20}>
-      <TileLayer
-        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-      />
-      <Marker position={center}/>
-    </Map>
-  )
+class NavScreen extends React.Component{
+  render() {
+    const { position } = this.props;
+    if (!position) return null;
+    const center = [position.coords.latitude, position.coords.longitude]
+    return (
+      <Map center={center} zoom={20}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={center}/>
+      </Map>
+    )
+  }
 }
 
 export default NavScreen
