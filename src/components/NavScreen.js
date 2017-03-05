f8cbc788602ffc7095add595e9d6e7e5187bb983
Dane Johnson <dane_johnson@ymail.com>
2017-03-05 13:06:36 -0600

    Added map view

--- src/components/NavScreen.js
+++ src/components/NavScreen.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Map, Marker, TileLayer } from 'react-leaflet'
+import '../css/map.css'
 
-const NavScreen = ({position}) => (
-  <div>
-    {position &&
-      <div>
-        <span>{position.coords.latitude}</span>
-        <span>{position.coords.longitude}</span>
-        <span>{position.timestamp}</span>
-      </div>
-    }
-  </div>
-)
+const NavScreen = ({position}) => {
+  if (!position) return null;
+  const center = [position.coords.latitude, position.coords.longitude]
+  return (
+    <Map center={center} zoom={20}>
+      <TileLayer
+        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={center}/>
+    </Map>
+  )
+}
 
 export default NavScreen
