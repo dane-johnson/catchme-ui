c15876b27589e1f3ac2be037105be00975903e30
Dane Johnson <dane_johnson@ymail.com>
2017-03-28 21:09:26 -0500

    Throttled position updates

--- src/lib/geowatcher.js
+++ src/lib/geowatcher.js
@@ -1,8 +1,11 @@
+import _ from 'lodash'
 import { updatePosition } from '../actions'
 
 const registerListener = (dispatch) => {
+  //At most, dispatch once a second.
+  const throttledDispatch = _.throttle(dispatch, 1000) //At most, dispatch once a second.
   navigator.geolocation.watchPosition((position) => {
-    dispatch(updatePosition(position))
+    throttledDispatch(updatePosition(position))
   }, null, {enableHighAccuracy: true});
 }
 
